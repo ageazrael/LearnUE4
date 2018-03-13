fc1b8d1fe42a192a3ee4d0fa7e190b07784d53ad
ageazrael <ageazrael@gmail.com>
2018-03-13 08:12:27 +0800

    Initial commit

---
+++ Source/LearnUE4/LearnUE4.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LearnUE4.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LearnUE4, "LearnUE4" );
